f0b8fb769eada4ebd5cdf8934eafbdcb9b90e8c1
Rushil Dandamudi <rushilcd@umd.edu>
2024-09-19 22:12:29 -0400

    started merge procedure

--- main.py
+++ main.py
@@ -4,8 +4,119 @@ import cirq
 import cirq.contrib.qasm_import
 
 DEFAULT_QUBITS_PER_NODE = 2
+VERBOSE = True
+
+def dict_append(d, key, append_value):
+    if key not in d:
+        d[key] = []
+    d[key].append(append_value)
+ 
+
+# subroutine for aggregating blocks
+def merge(left_block, right_block, pairs, qubit_ops):
+    left_pair = pairs[left_block[-1]]
+    right_pair = pairs[right_block[0]]
+
+    # check commutation rules and in-between gates to see if blocks can merge
+    q2_l, is_ctrl_l, _, op_idx_l = left_pair
+    q2_r, is_ctrl_r, _, op_idx_r = right_pair
+
+    for op_idx in range(op_idx_l + 1, op_idx_r):
+        curr_op = qubit_ops[op_idx]
+
+
+    return True, left_block + right_block
 
 def aggregate(in_circ, node_map, node_array=None):
+
+    # identify qubit-node pair with most remote gates
+    rem_pairs = dict()
+    qubit_ops = dict()
+
+    for layer_idx, layer in enumerate(in_circ):
+        for op in layer:
+            # skip if operation is single-qubit (assumming no X-qubit gates for X>2)
+            if len(op.qubits) != 2:
+                continue
+            
+            ctrl, targ = op.qubits
+            targ_node = node_map(targ)
+            ctrl_node = node_map(ctrl)
+
+            # skip if controlled operation is local
+            if ctrl_node == targ_node:
+                continue
+
+            dict_append(qubit_ops, ctrl, (targ, True))
+            dict_append(qubit_ops, targ, (ctrl, False))
+
+            # appending useful information
+            dict_append(rem_pairs, (ctrl, targ_node), (targ,True,layer_idx, len(qubit_ops[ctrl])-1) )
+            dict_append(rem_pairs, (targ, ctrl_node), (ctrl,False,layer_idx, len(qubit_ops[targ])-1) )
+
+   
+    # max_pair_len = -1
+    # max_pair = None
+    # for pair, num_pairs in rem_pairs.items():
+    #     if max_pair_len < len(num_pairs):
+    #         max_pair_len = len(num_pairs)
+    #         max_pair = pair
+    pair_queue = sorted(rem_pairs, key = lambda p: len(rem_pairs[p]), reverse=True)
+    if len(pair_queue) == 0:
+        return in_circ.copy()
+
+    max_pair_len = len(rem_pairs[pair_queue[0]])
+    if VERBOSE:
+        print(f'qubit node {pair_queue[0]} has most interaction: {max_pair_len} remote gates')
+
+    ## ITERATIVE REFINEMENT
+    for q1, node in pair_queue:
+        ## PREPROCESSING (find communication blocks)
+        pairs = rem_pairs[(q1, node)]
+        visited_pairs = set() # pairs that have been added to a block
+        comm_blocks = []
+        for idx in range(len(pairs)):
+            if idx in visited_pairs:
+                continue
+
+            _, _, layer_idx, _ = pairs[idx]
+            visited_pairs.add(idx)
+            comm_blocks.append([idx])
+
+            for idx_next in range(idx+1, len(pairs)):
+                _, _, layer_idx_next, _ = pairs[idx_next]
+                if layer_idx_next > layer_idx + 1:
+                    break
+
+                # consecutive pairs
+                visited_pairs.add(idx_next)
+                comm_blocks[-1].append(idx_next)
+                layer_idx += 1 # next idx becomes current
+
+        if VERBOSE:
+            print(f'{(q1,node)} consecutive blocks: {comm_blocks}') # debug
+
+        
+        ## LINEAR MERGE
+        visited_blocks = set()
+        for idx, block in enumerate(comm_blocks):
+            if idx in visited_blocks:
+                continue
+                
+            for idx_next in range(idx+1, len(comm_blocks)):
+                did_merge, blocks = merge(block, comm_blocks[idx_next], pairs, qubit_ops)
+                if not did_merge:
+                    break
+                visited_blocks.add(idx_next)
+
+
+
+
+
+
+                
+            
+                
     return in_circ.copy()
 
 
